8a7e22959b854f36fbbe1325c3540ac96a7424a7
Larataiwo <omolaraolutaiwo@gmail.com>
2024-06-19 11:16:19 +0100

    Add logic for one round of game

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rock_paper_scissors
+Create  a function that takes in computer choice and human choice
+Create a logic for one round of game and a console.log message
+Add variable that keep count of human and computer score setting it to zero
+Increment counter by each win/loss
+Create a function that include the one round of game which plays the round five times 
+Add a statement for counter to determine winner 
+Console.log winner 
--- script.js
+++ script.js
@@ -11,3 +11,23 @@ function getHumanChoice() {
         return input;
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+    if((humanChoice == "rock" && computerChoice == "scissors") || 
+    (humanChoice == "scissors" && computerChoice == "paper") || 
+    (humanChoice == "paper" && computerChoice == "rock")) {
+        humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice} `);
+    
+    } else if  ((computerChoice == "rock" && humanChoice == "scissors") || 
+    (computerChoice == "scissors" && humanChoice == "paper") || 
+    (computerChoice == "paper" && humanChoice == "rock")) {
+           computerScore++;
+            console.log(`You lose. ${computerChoice} beats ${humanChoice} `);
+        
+    } else if((computerChoice == "rock" && humanChoice == "rock") || 
+    (computerChoice == "paper" && humanChoice == "paper") ||
+    (computerChoice == "scissors" && humanChoice == "scissors")){
+            console.log(`It's a tie! ${humanChoice} is the same as ${computerChoice} `);
+        }
+    } 
