bd304bf6fbabfec019e02a5c45c7f60e7a63c62c
Larataiwo <omolaraolutaiwo@gmail.com>
2024-06-22 15:34:22 +0100

    Add playgame
    Add playgame to play a maximum of 5 games and return the winner. Edited the gethumanchoice to convert uppercase inputs to all lowercase

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let input = prompt("Enter rock, paper or scissors");
+    let humanInput = input.toLowerCase();
     const choice = ["rock","paper","scissors"];
-    if(input == choice[0] || input == choice[1] || input == choice[2]) {
-        return input;
+    if(humanInput == choice[0] || humanInput == choice[1] || humanInput == choice[2]) {
+        return humanInput; 
     }
 }
 
@@ -31,3 +32,29 @@ function playRound(humanChoice, computerChoice) {
             console.log(`It's a tie! ${humanChoice} is the same as ${computerChoice} `);
         }
     } 
+
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playGame() {
+    console.log("Welcome");
+   for(let i = 0; i < 5; i++) {
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+   } 
+   if(humanScore > computerScore) {
+    console.log(`You win! You win by ${humanScore - computerScore} points` );
+
+   } else if(humanScore < computerScore) {
+    console.log(`You lose. You lose by ${computerScore - humanScore} points`);
+
+   } else if(humanScore == computerScore){
+    console.log("You Draw");
+   }
+  } 
+
+   playGame(); 
+
